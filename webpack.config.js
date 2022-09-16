f00e7dc98e977c4403ee225bf41bafc4bbaba715
billums123 <wthest123@gmail.com>
2022-09-16 16:08:19 -0600

    dev branch foundation set up

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV, //development or production
+  entry: "./client/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  //   plugins: [new MiniCssExtractPlugin({filename: 'trine.css'}), new HtmlWebpackPlugin({
+  plugins: [
+    new HtmlWebpackPlugin({
+      // hash: true,
+      title: "Development",
+      template: "./index.html", //THIS IS IMPORTANT TO GET INDEX.HTML TO LOAD AFTER YOU ADD DEV SERVER!!!!!
+    }),
+  ],
+  devServer: {
+    static: {
+      publicPath: "/build",
+      directory: path.resolve(__dirname, "build"),
+    },
+    proxy: {
+      "/api/**": "http://localhost:3000",
+    },
+  },
+};
